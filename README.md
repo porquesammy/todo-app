4e83b3696c56cfa9ca1232d89270366bb9f998f1
porquesammy <craigcardoni@gmail.com>
2019-04-13 15:02:35 -0700

    fix js to prevent default

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Start of a todo application I will be working on.<br/>
 <br/>
-<img src="https://i.gyazo.com/c0b96f8d82381c8ae03959dfe29fdf44.gif" alt="Demo of todo app" width="460"/>
+<img src="https://i.gyazo.com/e0ac027a7550ed207146614dcba7c106.gif" alt="Demo of todo app" width="460"/>
 <br/><br/>
 Needs:<br/>
 Delete Icon and functionality<br/>
--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const input = document.getElementById('task-input');
 const addBtn = document.getElementById('add-btn');
 const ulEl = document.getElementById('task-ul');
 
-addBtn.addEventListener('click', function () {
+addBtn.addEventListener('click', function (e) {
+    e.preventDefault();
     let addTasksText = input.value;
     let currentUl = ulEl.innerHTML;
     if (0 < input.value.length && input.value !== " ") {
