bc215ab4cf9894d2424d12b29bd27b89a5fb7b4c
porquesammy <craigcardoni@gmail.com>
2019-04-13 20:12:08 -0700

    add enter keydown listener, add new background, add styling fix

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
 Start of a todo application I will be working on.<br/>
 <br/>
-<img src="https://i.gyazo.com/e0ac027a7550ed207146614dcba7c106.gif" alt="Demo of todo app" width="460"/>
+<img src="https://i.gyazo.com/7f7ab66b6a0d24ca77cf038a7565ca96.gif" alt="Demo of todo app" width="460"/>
 <br/><br/>
 Needs:<br/>
 Delete Icon and functionality<br/>
 Edit Icon and functionality<br/>
 Favicon<br/>
 Header resize for smaller viewports<br/>
+<del>Add keydown listener to submit</del><br/>>
+
+
 
--- script.js
+++ script.js
@@ -6,10 +6,20 @@ const ulEl = document.getElementById('task-ul');
 
 addBtn.addEventListener('click', function (e) {
     e.preventDefault();
+    updateList();
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+        updateList();
+    }
+});
+
+const updateList = function () {
     let addTasksText = input.value;
     let currentUl = ulEl.innerHTML;
     if (0 < input.value.length && input.value !== " ") {
-        ulEl.innerHTML = currentUl + " <li>" + addTasksText + "</li>";  
-        input.value = "";    
+        ulEl.innerHTML = currentUl + " <li>" + addTasksText + "</li>";
+        input.value = "";
     }
-});
+};
